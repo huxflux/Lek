133618de7d9025967e7159ad6da5757cd55b7541
huxflux <johnis@student.hist.no>
2013-09-30 22:17:41 +0200

    Ok

---
+++ src/Sick.java
@@ -0,0 +1,18 @@
+/*
+ * Sick.java
+ *
+ * Yeye, bare testings!
+ *
+ * huxfucks
+ */
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
+public class Sick {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
