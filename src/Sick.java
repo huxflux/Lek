21e5afec07cf72e23677effb48afc07260ada54e
huxflux <johnis@student.hist.no>
2013-09-30 22:53:38 +0200

    Ok

--- src/Sick.java
+++ src/Sick.java
@@ -9,10 +9,26 @@
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
 public class Sick {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final int       SCREEN_WIDTH = 800;
+        final int       SCREEN_HEIGHT = 600;
+
+        try {
+            Display.setDisplayMode(new DisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT));
+            Display.create();
+            Display.setTitle("Aurora Borealis");
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+            Display.update();
+        }
+        Display.destroy();
     }
 }
