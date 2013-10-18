4586ce1ed1261e200e8d8241f0ee5fb6c1c999c0
huxflux <johnis@student.hist.no>
2013-10-18 10:33:53 +0200

    Toillj

--- src/Sick.java
+++ src/Sick.java
@@ -7,41 +7,48 @@
  */
 
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.ContextAttribs;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.PixelFormat;
+        import org.lwjgl.opengl.ContextAttribs;
+        import org.lwjgl.opengl.Display;
+        import org.lwjgl.opengl.DisplayMode;
+        import org.lwjgl.input.Keyboard;
+        import org.lwjgl.opengl.GL11;
+        import org.lwjgl.opengl.GL15;
+        import org.lwjgl.opengl.GL20;
+        import org.lwjgl.opengl.GL30;
+        import org.lwjgl.opengl.PixelFormat;
+        import org.lwjgl.util.glu.GLU;
 
-public class Sick {
-    public static void main(String[] args) {
-        final int       SCREEN_WIDTH = 800;
-        final int       SCREEN_HEIGHT = 600;
-        final int       FPS = 60;
+        public class Sick {
+            public static void main(String[] args) {
+                final int       SCREEN_WIDTH = 800;
+                final int       SCREEN_HEIGHT = 600;
+                final int       FPS = 60;
+                int             vaoId = 0;
+                int             vboId = 0;
+                int             vertexCount = 0;
 
-        PixelFormat     pixelFormat = new PixelFormat();
-        ContextAttribs  contextAttribs = new ContextAttribs(3, 2);
+                PixelFormat     pixelFormat = new PixelFormat();
+                ContextAttribs  contextAttribs = new ContextAttribs(3, 2);
 
-        contextAttribs.withForwardCompatible(true);
-        contextAttribs.withProfileCore(true);
+                contextAttribs.withForwardCompatible(true);
+                contextAttribs.withProfileCore(true);
 
-        try {
-            Display.setDisplayMode(new DisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT));
-            Display.setTitle("Perdurabo");
-            Display.create(pixelFormat, contextAttribs);
-        } catch (LWJGLException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
+                try {
+                    Display.setDisplayMode(new DisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT));
+                    Display.setTitle("Perdurabo");
+                    Display.create(pixelFormat, contextAttribs);
+                } catch (LWJGLException e) {
+                    e.printStackTrace();
+                    System.exit(-1);
+                }
 
-        System.out.println(GL11.glGetString(GL11.GL_VERSION));
+                System.out.println(GL11.glGetString(GL11.GL_VERSION));
 
-        while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
-            Display.sync(FPS);
-            Display.update();
-        }
+                while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+                    Display.sync(FPS);
+                    Display.update();
+                }
 
-        Display.destroy();
+                Display.destroy();
     }
 }
